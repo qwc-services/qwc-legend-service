e0f5b0c0c8fc068b67b98da9db1f29ed19ac0684
Hussein Kabbout <hussein.kabbout@hotmail.com>
2021-03-25 16:05:20 +0100

    Fix unittests

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -29,42 +29,28 @@ class ApiTestCase(unittest.TestCase):
 
     def test_default_legendgraphic(self):
         params = {
-            'service': 'WMS',
-            'version': '1.3.0',
-            'request': 'GetLegendGraphic',
             'format': 'image/png',
-            'layer': 'test_poly',
-            'crs': 'EPSG%3A2056',
-            'dpi': '96',
-            'width': '64',
-            'height': '64',
-            'bbox': '2606082.333333333%2C1233466.3333333333%2C2633175.666666666%2C1245234.9999999998',
+            'layer': 'edit_polygons'
         }
-        response = self.app.get('/somap?' + urlencode(params), headers=self.jwtHeader())
+        response = self.app.get('/qwc_demo?' + urlencode(params), headers=self.jwtHeader())
         self.assertEqual(200, response.status_code, "Status code is not OK")
-        data = json.loads(response.data)
-        self.assertEqual('somap', data['path'], 'Print project name mismatch')
-        self.assertEqual('GET', data['method'], 'Method mismatch')
-        get_params = data['params']
-        for param in params.keys():
-            self.assertTrue(param in get_params, "Parameter %s missing in response" % param)
-            self.assertEqual(get_params[param], str(params[param]), "Parameter %s mismatch" % param)
+
+        success = False
+        try:
+            img = Image.open(BytesIO(response.data))
+            success = True
+        except Exception as e:
+            print(e)
+            success = False
+        self.assertTrue(success, "Response is not a valid image")
 
     def test_custom_legendgraphic(self):
-        # NOTE: test_points has a custom legend graphic image, the response is the actual image, and not the response of the dummy qgis server
+        # NOTE: edit_points has a custom legend graphic image, the response is the actual image, and not the response of the dummy qgis server
         params = {
-            'service': 'WMS',
-            'version': '1.3.0',
-            'request': 'GetLegendGraphic',
             'format': 'image/png',
-            'layer': 'test_points',
-            'crs': 'EPSG%3A2056',
-            'dpi': '96',
-            'width': '64',
-            'height': '64',
-            'bbox': '2606082.333333333%2C1233466.3333333333%2C2633175.666666666%2C1245234.9999999998',
+            'layer': 'edit_points'
         }
-        response = self.app.get('/somap?' + urlencode(params), headers=self.jwtHeader())
+        response = self.app.get('/qwc_demo?' + urlencode(params), headers=self.jwtHeader())
         self.assertEqual(200, response.status_code, "Status code is not OK")
 
         success = False
